6ab7f8506075392e2791fe1637995c6cad2f3d6a
Gepangz <501167129@qq.com>
2017-05-22 14:07:32 +0800

    v.0.1

---
+++ src/components/Test.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    Value: {{ test }}, count is {{ evenOrOdd }}.
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  computed: mapGetters([
+    'test',
+    'evenOrOdd'
+  ]),
+  methods: mapActions([
+    'increment',
+    'decrement'
+  ])
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = { count: 0 }
+
+const mutations = { increment (state) { state.count++ }, decrement (state) { state.count-- } }
+
+const actions = { increment: ({commit}) => commit('increment'), decrement: ({commit}) => commit('decrement') }
+
+const getters = { test: state => state.count, evenOrOdd: state => state.count % 2 === 0 ? 'even' : 'odd' }
+
+export default new Vuex.Store({state, getters, actions, mutations})
